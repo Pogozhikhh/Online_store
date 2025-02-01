7664ccabb66622e300801d02b3122667830d5a2b
Pogozhikhh <Pogozhikhh@yandex.ru>
2025-02-01 16:47:30 +0300

    Complete homework 14_1?

--- test/test_Product.py
+++ test/test_Product.py
@@ -1,15 +1,11 @@
-import pytest
-
-from src.Product import Product
-
-
-@pytest.fixture
-def prod_1():
-    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-
-
 def test_init(prod_1):
     assert prod_1.name == "Iphone 15"
     assert prod_1.description == "512GB, Gray space"
     assert prod_1.price == 210000.0
     assert prod_1.quantity == 8
+
+def test_init_2(prod_2):
+    assert prod_2.name == "Book"
+    assert prod_2.description == "paper"
+    assert prod_2.price == 200.0
+    assert prod_2.quantity == 100
